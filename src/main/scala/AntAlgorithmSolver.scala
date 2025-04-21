5a80d530654c1d13278f824ed1ecc77cc123259a
Jakub Kurcwald <jakub.kurcwald@gmail.com>
2025-04-21 18:29:48 +0200

    Printing

--- src/main/scala/AntAlgorithmSolver.scala
+++ src/main/scala/AntAlgorithmSolver.scala
@@ -26,7 +26,7 @@ object AntAlgorithmSolver extends App {
   }
 
   OParser.parse(parser, args, Config()) match {
-    case Some(config) => new Solver(cities.toSet, config).solve.cities.map(c => c.id)
+    case Some(config) => println(new Solver(cities.toSet, config).solve.cities.map(c => c.id))
     case _ => println("Podano nieprawidłowe argumenty! Użyj --help, aby zobaczyć dostępne.")
   }
 }
